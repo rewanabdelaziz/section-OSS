2ff082c6e7eed2c1dfd20e60cd4dc99e70ad95ff
Rewan Abd-Elaziz <95918964+rewanabdelaziz@users.noreply.github.com>
2022-12-13 20:42:59 +0200

    add main file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Rewan;
+public class Rewan{
+    private static void Main()
+    {  
+         //task 1
+        Console.WriteLine("Enter a string");
+         string str=Console.ReadLine();
+         var rev="";
+         int j;
+         for(j=str.Length-1; j>=0 ;j--){
+            rev+=str[j];
+         }
+          Console.WriteLine(rev);
+
+       
+    }
+}
