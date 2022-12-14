010d1323739bd8af6f78649a7282a4a1ba92dc4c
Rewan Abd-Elaziz <95918964+rewanabdelaziz@users.noreply.github.com>
2022-12-14 22:29:12 +0200

    do task2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 namespace Rewan;
+
+//task2
+
 public abstract class Person{
     public string Name;
     public int Age;
@@ -12,7 +15,19 @@ public abstract class Person{
      }
      public abstract void print();
 }
+//can't create object of abstract class Person 
+public class Person2 :Person{
+    public Person2(string name,int age) : base (name,age)
+    {
+    
+    }
 
+    public override void print()
+     {
+        Console.WriteLine($"My name is {Name} and my age is {Age}");
+     }
+
+}
 
 public class Student : Person
 {
@@ -34,6 +49,24 @@ public class Student : Person
      }
 
 }
+
+public class Staff : Person
+{
+public double Salary;
+public int JoinYear;
+
+public Staff(string name,int age,double salary, int joinYear) : base(name,age){
+    Salary=salary;
+    JoinYear=joinYear;
+}
+
+public override void print()
+     {
+        Console.WriteLine($"My name is {Name}, my age is {Age}, and my Salary is {Salary}");
+     }
+}
+
+
 public class Database
 {
     private int _currentIndex;
@@ -42,37 +75,124 @@ public class Database
     //create array --> name is people and type is Person
     public Person[] People =new Person[50];
 
-    public void AddStudent(Student student)
-    {   
-        //if (cuurentIndex == _count-1) return;
-        People[_currentIndex++]= student;
+     public void AddStudent(Student student)
+     {   
+         //if (cuurentIndex == _count-1) return;
+         People[_currentIndex++]= student;
+     }
+
+    public void AddStaff(Staff staff)
+    {
+         People[_currentIndex++]= staff;
+    }
+
+    public void AddPerson(Person person)
+    {
+         People[_currentIndex++]= person;
+    }
+
+    public void PrintAll(){
+        for( var i=0 ; i<_currentIndex ; i++)
+        {
+            People[i].print();
+        }
     }
 }
+
 
 public class Rewan{
     private static void Main()
     {  
         var database = new Database();
+
+        while( true){
+            Console.WriteLine("1)student  2) staff  3)person   4)all people");
+
+            Console.Write("Option: ");
+
+            var option =Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                   Console.Write("Name : ");
+
+                   var name =Console.ReadLine();
+
+                   Console.Write("Age : ");
+
+                   var age =Convert.ToInt32(Console.ReadLine());
         
-        Console.Write("Name : ");
+                   Console.Write("Year : ");
 
-        var name =Console.ReadLine();
-
-        Console.Write("Age : ");
-
-        var age =Convert.ToInt32(Console.ReadLine());
+                   var year =Convert.ToInt32(Console.ReadLine());
         
-        Console.Write("Year : ");
+                   Console.Write("Gpa : ");
 
-        var year =Convert.ToInt32(Console.ReadLine());
+                   var gpa =Convert.ToSingle(Console.ReadLine());
+
+                   var student= new Student(name,age,year,gpa);
+
+                   database.AddStudent(student);
+
+                    break;
+                
+                case 2:
+
+                    Console.Write("Name : ");
+
+                   var name2 =Console.ReadLine();
+
+                   Console.Write("Age : ");
+
+                   var age2 =Convert.ToInt32(Console.ReadLine());
+
+                   Console.Write("Salary : ");
+
+                   var salary =Convert.ToSingle(Console.ReadLine());
+         
+                  Console.Write("Join Year : ");
+
+                  var joinYear =Convert.ToInt32(Console.ReadLine());      
+
+                  var staff = new Staff(name2,age2,salary,joinYear);
+
+                  database.AddStaff(staff);
+
+                  break;
+
+                case 3:
+
+                    Console.Write("Name : ");
+
+                   var name3 =Console.ReadLine();
+
+                   Console.Write("Age : ");
+
+                   var age3 =Convert.ToInt32(Console.ReadLine());
+
+                   var person= new Person2(name3,age3);
         
-        Console.Write("Gpa : ");
+                   database.AddPerson(person);
 
-        var gpa =Convert.ToSingle(Console.ReadLine());
+                    break;
 
-        var student= new Student(name,age,year,gpa);
+                case 4:
+
+                    database.PrintAll();
+                    break;
+
+                default:
+                    return;
+            }
+        }
         
-        database.AddStudent(student);
+        
+         
+       
+
+        
 
 
 
